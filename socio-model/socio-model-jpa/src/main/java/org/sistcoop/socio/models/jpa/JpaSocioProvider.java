cc7f45858aadbbb190f48ff8cdd38a13ab442c7f
Carlos <carlosthe19916@gmail.com>
2015-08-24 11:31:20 -0500

    1

--- socio-model/socio-model-jpa/src/main/java/org/sistcoop/socio/models/jpa/JpaSocioProvider.java
+++ socio-model/socio-model-jpa/src/main/java/org/sistcoop/socio/models/jpa/JpaSocioProvider.java
@@ -1,6 +1,7 @@
 package org.sistcoop.socio.models.jpa;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashSet;
 import java.util.List;
@@ -18,6 +19,7 @@ import javax.persistence.TypedQuery;
 import org.sistcoop.socio.models.SocioModel;
 import org.sistcoop.socio.models.SocioProvider;
 import org.sistcoop.socio.models.enums.EstadoCuentaAporte;
+import org.sistcoop.socio.models.enums.Frecuencia;
 import org.sistcoop.socio.models.enums.TipoPersona;
 import org.sistcoop.socio.models.jpa.entities.ComisionSocioAsignadaEntity;
 import org.sistcoop.socio.models.jpa.entities.ComisionSocioEntity;
@@ -61,6 +63,13 @@ public class JpaSocioProvider extends AbstractHibernateStorage implements SocioP
         queryComisiones.setParameter("estado", true);
         List<ComisionSocioEntity> resultComisiones = queryComisiones.getResultList();
 
+        List<ComisionSocioEntity> resultComisionesDeInicio = new ArrayList<ComisionSocioEntity>();
+        for (ComisionSocioEntity comisionSocioEntity : resultComisiones) {
+            if (comisionSocioEntity.getFrecuencia().equals(Frecuencia.UNICO_INICIO)) {
+                resultComisionesDeInicio.add(comisionSocioEntity);
+            }
+        }
+
         // Crear
         Set<ComisionSocioAsignadaEntity> comisiones = new HashSet<>();
         for (ComisionSocioEntity comisionProgramada : resultComisiones) {
@@ -74,8 +83,8 @@ public class JpaSocioProvider extends AbstractHibernateStorage implements SocioP
         }
 
         CuentaAporteEntity cuentaAporteEntity = new CuentaAporteEntity();
-        // POR_ACTIVAR hasta que pague las comisionesadmin
-        cuentaAporteEntity.setEstado(resultComisiones.isEmpty() ? EstadoCuentaAporte.ACTIVO
+        // POR_ACTIVAR hasta que pague las comisiones
+        cuentaAporteEntity.setEstado(resultComisionesDeInicio.isEmpty() ? EstadoCuentaAporte.ACTIVO
                 : EstadoCuentaAporte.POR_ACTIVAR);
         cuentaAporteEntity.setMoneda(cuentaAporteMonedaEntity.getMoneda());
         cuentaAporteEntity.setNumeroCuenta("");
@@ -87,7 +96,7 @@ public class JpaSocioProvider extends AbstractHibernateStorage implements SocioP
         socioEntity.setNumeroDocumento(numeroDocumento);
         socioEntity.setFechaInicio(Calendar.getInstance().getTime());
         // No es considerado socio hasta que pague las comisiones
-        socioEntity.setEstado(resultComisiones.isEmpty() ? true : false);
+        socioEntity.setEstado(resultComisionesDeInicio.isEmpty() ? true : false);
         socioEntity.setCuentaAporte(cuentaAporteEntity);
         socioEntity.setComisione(comisiones);
 
@@ -97,38 +106,58 @@ public class JpaSocioProvider extends AbstractHibernateStorage implements SocioP
 
     @Override
     public boolean remove(SocioModel socioModel) {
-        // TODO Auto-generated method stub
-        return false;
+        SocioEntity socioEntity = em.find(SocioEntity.class, socioModel.getId());
+        if (socioEntity == null) {
+            return false;
+        }
+        em.remove(socioEntity);
+        return true;
     }
 
     @Override
     public SocioModel findById(String id) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public SocioModel findByTipoNumeroDocumento(String tipoDocumento, String numeroDocumento) {
-        // TODO Auto-generated method stub
-        return null;
+        SocioEntity socioEntity = this.em.find(SocioEntity.class, id);
+        return socioEntity != null ? new SocioAdapter(em, socioEntity) : null;
     }
 
     @Override
     public List<SocioModel> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        TypedQuery<SocioEntity> query = em.createNamedQuery("SocioEntity.findAll", SocioEntity.class);
+        List<SocioEntity> list = query.getResultList();
+        List<SocioModel> result = new ArrayList<>();
+        for (SocioEntity entity : list) {
+            result.add(new SocioAdapter(em, entity));
+        }
+        return result;
     }
 
     @Override
-    public SearchResultsModel<SocioModel> search(SearchCriteriaModel searchCriteriaBean) {
-        // TODO Auto-generated method stub
-        return null;
+    public SearchResultsModel<SocioModel> search(SearchCriteriaModel criteria) {
+        SearchResultsModel<SocioEntity> entityResult = find(criteria, SocioEntity.class);
+
+        SearchResultsModel<SocioModel> modelResult = new SearchResultsModel<>();
+        List<SocioModel> list = new ArrayList<>();
+        for (SocioEntity entity : entityResult.getModels()) {
+            list.add(new SocioAdapter(em, entity));
+        }
+        modelResult.setTotalSize(entityResult.getTotalSize());
+        modelResult.setModels(list);
+        return modelResult;
     }
 
     @Override
-    public SearchResultsModel<SocioModel> search(SearchCriteriaModel searchCriteriaBean, String FfilterText) {
-        // TODO Auto-generated method stub
-        return null;
+    public SearchResultsModel<SocioModel> search(SearchCriteriaModel criteria, String filterText) {
+        SearchResultsModel<SocioEntity> entityResult = findFullText(criteria, SocioEntity.class, filterText,
+                "tipoDocumento", "numeroDocumento");
+
+        SearchResultsModel<SocioModel> modelResult = new SearchResultsModel<>();
+        List<SocioModel> list = new ArrayList<>();
+        for (SocioEntity entity : entityResult.getModels()) {
+            list.add(new SocioAdapter(em, entity));
+        }
+        modelResult.setTotalSize(entityResult.getTotalSize());
+        modelResult.setModels(list);
+        return modelResult;
     }
 
 }
